bf3e330a5247f593e9432a6858b775a1799b0b23
Anoop Tiwari <tiwarianoop2@gmail.com>
2014-12-07 09:35:02 +0530

    m5thread of Gem5
    I have build binary of "fft","lu" ,"fmm" of splash2 benchmark in
    "m5thread\tests" folder.

---
+++ tests/Parallel.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define NTHREADS 5
+int y=0;
+void *myFun(void *x)
+{
+  int tid;
+  tid = *((int *) x);
+y=y+5;
+  printf("Hi from thread %d!\n", tid);
+printf("y from thread %d,%d!\n",tid,y);
+  return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+  pthread_t threads[NTHREADS];
+  int thread_args[NTHREADS];
+  int rc, i;
+
+  /* spawn the threads */
+  for (i=0; i<NTHREADS; ++i)
+    {
+      thread_args[i] = i;
+      printf("spawning thread %d\n", i);
+      rc = pthread_create(&threads[i], NULL, myFun, (void *) &thread_args[i]);
+    }
+
+  /* wait for threads to finish */
+  for (i=0; i<NTHREADS; ++i) {
+    rc = pthread_join(threads[i], NULL);
+  }
+
+  return 1;
+}
